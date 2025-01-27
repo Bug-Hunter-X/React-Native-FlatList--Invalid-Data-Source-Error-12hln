6b490123f4b0a3177e0ecd9a5becc8b71a0c480b
BugHunter-X <askzenai@gmail.com>
2025-01-27 05:44:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the FlatList component in React Native and providing an invalid or improperly formatted data source.  Specifically, it happens when the data prop is not an array or contains non-object items when using the `renderItem` prop that expects an object with specific keys.
